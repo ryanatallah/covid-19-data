dcfceed6c3111f5329402450487fd2f77d405ede
Ryan Atallah <ryan@atallah.family>
2020-04-01 23:42:02 -0700

    Changes

--- transform.py
+++ transform.py
@@ -1,9 +1,25 @@
-from matplotlib import pyplot as plt
-import csv
 import pandas as pd
 
+
+def to_record(first_day, row, prev_day, key):
+    delta = row['date']-first_day
+    new = row[key] - prev_day[key]
+    record = {
+        'date': row['date'],
+        'state': row['state'],
+        'fips': row['fips'],
+        'day_num': delta.days,
+        'new': new,
+        'value': row[key],
+        'type': key
+    }
+    if prev_day[key] > 0:
+        record['growth'] = new / float(prev_day[key])
+    return record
+
+
 def transform_data(in_file, out_file, min_cases):
-    data = pd.read_csv(in_file);
+    data = pd.read_csv(in_file)
     data['date'] = pd.to_datetime(data['date'])
     new_data = []
     first_day_by_fips = {}
@@ -17,21 +33,12 @@ def transform_data(in_file, out_file, min_cases):
             continue
         if row['cases'] >= min_cases:
             first_day = first_day_by_fips.get(fips)
-            if first_day == None:
+            if first_day is None:
                 first_day = row['date']
                 first_day_by_fips[fips] = first_day
-            delta = row['date']-first_day
-            new_row = row
-            new_row['day_num'] = delta.days
-            new_cases = row['cases'] - prev_day['cases']
-            new_row['new_cases'] = new_cases
-            if prev_day['cases'] > 0:
-                new_row['cases_growth'] = new_cases / float(prev_day['cases'])
-            new_deaths = row['deaths'] - prev_day['deaths']
-            new_row['new_deaths'] = new_deaths
-            if prev_day['deaths'] > 0:
-                new_row['deaths_growth'] = new_deaths / float(prev_day['deaths'])
-            new_data.append(new_row)
+
+            new_data.append(to_record(first_day, row, prev_day, 'cases'))
+            new_data.append(to_record(first_day, row, prev_day, 'deaths'))
         prev_day_by_fips[fips] = row
 
     df = pd.DataFrame(new_data)
@@ -39,4 +46,3 @@ def transform_data(in_file, out_file, min_cases):
 
 
 transform_data('us-states.csv', 'us-states-normalized.csv', 100)
-transform_data('us-counties.csv', 'us-counties-normalized.csv', 48)
